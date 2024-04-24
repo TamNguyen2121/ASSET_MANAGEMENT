cf9231e28d6e2d45c858323ee3d4bdcb474b9440
Hoa1210 <luonghoa121002@gmail.com>
2024-04-24 23:23:02 +0700

    clear case 2

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -2,10 +2,12 @@
     <div class="limiter">
         <div class="container-login100">
             <div class="wrap-login100">
-                <div class="login100-more"
-                    style="background-image: url('{{asset('auth/images/bg.jpg')}}');">
+                <div class="login100-more" style="background-image: url('{{asset('auth/images/bg.jpg')}}');">
                 </div>
                 <form class="login100-form validate-form" wire:submit="loginProcess">
+                    <div style="display: flex;justify-content: center; padding-bottom:50px">
+                        <img src="{{ asset('admin_dashbroad/assets/images/logos/logo-light.jpg') }}" width="300px" alt="" />
+                    </div>
                     <span class="login100-form-title p-b-43">
                         Đăng nhập
                     </span>
@@ -26,18 +28,18 @@
                         </button>
                     </div>
                     @if ($show == true)
-                        <div class="alert alert-danger mt-3" role="alert">
-                            <div class="d-flex justify-content-between">
-                                <div class="">{{ $errors }}</div>
-                                <div class="">
-                                    <i class="bi bi-x" wire:click='closeAlert()'></i>
-                                </div>
+                    <div class="alert alert-danger mt-3" role="alert">
+                        <div class="d-flex justify-content-between">
+                            <div class="">{{ $errors }}</div>
+                            <div class="">
+                                <i class="bi bi-x" wire:click='closeAlert()'></i>
                             </div>
                         </div>
+                    </div>
                     @endif
                 </form>
-
             </div>
         </div>
     </div>
 </div>
+
